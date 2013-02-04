e69831a235d4d5ae93d67d39a72c71677b080779
Andrej A Antonov <polymorphm@gmail.com>
2013-02-05 03:57:03 +0400

    fixup! routes order -- fixed

--- lib_world_news_web_app_2013_02_04/app.py
+++ lib_world_news_web_app_2013_02_04/app.py
@@ -57,6 +57,6 @@ def create_app(root=None, static_root=None):
     
     app.route('%s/<filename:path>' % static_root, callback=send_static)
     app.route('%s/' % root, callback=home_view.home_view)
-    app.route('%s/<path:path>' % root, callback=news_view.news_view)
+    app.route('%s/news/<path:path>' % root, callback=news_view.news_view)
     
     return app
